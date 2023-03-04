21ad08ef64e78060dc509f0cf446f26b4be2533d
RytisMark <rytismarkeviciuss@gmail.com>
2023-03-04 20:56:58 +0200

    new features, UI updated

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
 const gridMap = document.querySelector('.grid-map');
-numOfRows = 16;
-numOfCols = 16;
+const sizeSlider = document.querySelector('.size-slider');
+const sliderText = document.querySelector('.slider-text');
+const btnClear = document.querySelector('.btn-clear');
+let numOfRows = 16;
+let numOfCols = 16;
+let isDragOn;
+
+
+sizeSlider.oninput = function() {
+    sliderText.textContent = `${this.value} x ${this.value}`;
+    numOfRows = this.value;
+    numOfCols = this.value;
+    
+    removeGridMap();
+    createGridMap(numOfRows, numOfCols);
+}
+
+btnClear.addEventListener('click', () => {
+    removeGridMap();
+    createGridMap(numOfRows, numOfCols);
+});
+
+createGridMap(numOfRows, numOfCols);
 
 function createGridMap(rows, cols) {
     gridMap.style.setProperty('--grid-rows', rows);
     gridMap.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++) {
-      let grid = document.createElement("div");
-
-    //   grid.innerText = (i + 1);
-      grid.addEventListener('mousemove', (e) => {
-        e.target.classList.add('colored-grid');
-    });
+        let grid = document.createElement("div");
+        grid.addEventListener('mousedown', () => isDragOn = true);
+        grid.addEventListener('mousemove', (e) => {
+        if (isDragOn) {
+            e.target.classList.add('colored-grid-item');
+        }
+      });
+      grid.addEventListener('mouseup', () => isDragOn = false);
 
       gridMap.appendChild(grid).className = "grid-item";
     };
-  };
-  
-  createGridMap(numOfRows, numOfCols);
-  
+    gridMap.addEventListener('mouseleave', () => isDragOn = false);
+}
+
+function removeGridMap() {
+    while (gridMap.firstChild) {
+        gridMap.removeChild(gridMap.firstChild);
+    }
+}
